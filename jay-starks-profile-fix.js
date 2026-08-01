ee793b06fe16b399d2284ffce6b9d4ce45d9773c
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-01 18:10:30 +0100

    Use valid Jay Starks portrait on artist profile

---
+++ jay-starks-profile-fix.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+
+  const profile = window.NGS_ARTIST_PROFILES?.["jay-starks"];
+  if (!profile) return;
+
+  profile.image = "/assets/artists/jay-starks-portrait-v2.webp?v=20260801-2";
+  profile.imagePosition = "50% 38%";
+})();

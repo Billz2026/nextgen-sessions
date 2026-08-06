b8252023898a98868094a3710976cb22552e9837
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-06 13:16:27 +0100

    Add Darian Gayle portrait mapping

---
+++ darian-gayle-images.js
@@ -0,0 +1,8 @@
+window.NGS_ARTIST_IMAGES = Object.assign(window.NGS_ARTIST_IMAGES || {}, {
+  "darian-gayle": {
+    src: "/api/darian-portrait?v=20260806-darian1",
+    portrait: "/api/darian-portrait?v=20260806-darian1",
+    fallback: "/api/darian-portrait?v=20260806-darian1",
+    position: "50% 30%"
+  }
+});

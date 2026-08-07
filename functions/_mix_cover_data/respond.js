a6a7dfd30ec80579a61a8d9e05ad7d5592cea02f
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-07 22:45:19 +0100

    Add mix cover response helper

---
+++ functions/_mix_cover_data/respond.js
@@ -0,0 +1,12 @@
+export function webpResponse(base64Data) {
+  const binary = atob(base64Data);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i += 1) bytes[i] = binary.charCodeAt(i);
+  return new Response(bytes, {
+    headers: {
+      "Content-Type": "image/webp",
+      "Cache-Control": "public, max-age=31536000, immutable",
+      "X-Content-Type-Options": "nosniff"
+    }
+  });
+}

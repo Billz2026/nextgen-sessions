21ed42c933f0c5c069a4cbb9389b96f90c7448c2
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-25 14:08:02 +0100

    Use final Zion portrait across roster

--- zion-daley-image-fix.js
+++ zion-daley-image-fix.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const portrait = "/assets/artists/zion-daley-portrait-final.webp?v=20260825-zionfinal1";
+  const portrait = "/assets/artists/zion-daley-portrait-final.webp?v=20260825-zionfinal2";
   window.NGS_ARTIST_IMAGES = window.NGS_ARTIST_IMAGES && typeof window.NGS_ARTIST_IMAGES === "object"
     ? window.NGS_ARTIST_IMAGES
     : {};
